0ec2d08273f850384c57ee64bcce62b8dfffce92
Cameron Broomfield <brmcam003@myuct.ac.za>
2023-01-01 14:05:15 +0200

    tidy up

--- CreateGraph.py
+++ CreateGraph.py
@@ -1,54 +1,83 @@
+import os
 from pyvis.network import Network
 import networkx as nx
 
-net = Network(width='1000px', height='1000px', bgcolor='#222222', font_color='white')
-net.barnes_hut()
 
-graph_dict = {}
 
-adj_list = open('AdjacencyList.txt').readlines()
-# adj_list = ['2 aani 288, 353, 515, 1380, 4039, 4823, 5295, ']
-
-if len(adj_list[0]) < 3:
-    adj_list.pop(0)
-
-adj_list = adj_list[:1000]
-
-for i, line in enumerate(adj_list):
-    print('i:', i, end=' ')
+def load_adjacency_list(file_name):
+    file = open(file_name)
+    adj_list = file.readlines()
+    file.close()
+    # adj_list = ['2 aani 288, 353, 515, 1380, 4039, 4823, 5295, ']
     
-    first_index = line.index(' ') + 1
-    second_index = line.index(' ', first_index+1)
+    if len(adj_list[0]) < 3:
+        adj_list.pop(0)
     
-    num = line[:first_index]
-    name = line[first_index:second_index]
-    lst_str = line[second_index:]
-    
-    lst = lst_str.split(sep=', ')[:-1]
-    lst = [int(x) for x in lst]
-    
-    print('num:', num, 'name:', name, 'lst_str:', lst_str, 'list:', lst)
-    
-    graph_dict[name] = lst
-    
-    net.add_node(n_id=int(num), label=name, color='blue', size=30, physics=True)
+    return adj_list
 
 
-# for node in net.get_nodes():
-#     print('node:', node)
-
-
-for num, adj_nodes in enumerate(graph_dict.values()):
-    print(num)
-    for adj_node in adj_nodes:
-        # print('num', num, type(num), 'adj_node:', adj_node, type(adj_node))
-        # net.add_edge(net.get_node(num), net.get_node(adj_node))
-        try:
-            net.add_edge(num, adj_node, physics=False)
-        except:
-            pass    
+def create_adjacency_dictionary(adj_list):
     
+    graph_dict = {}
+    for i, line in enumerate(adj_list):
+        print('i:', i, end=' ')
+        
+        first_index = line.index(' ') + 1
+        second_index = line.index(' ', first_index+1)
+        
+        num = line[:first_index]
+        name = line[first_index:second_index]
+        lst_str = line[second_index:]
+        
+        lst = lst_str.split(sep=', ')[:-1]
+        lst = [int(x) for x in lst]
+        
+        print('num:', num, 'name:', name, 'lst_str:', lst_str, 'list:', lst)
+        
+        graph_dict[name] = lst
+    
+    return graph_dict
 
-net.show('graphs/graph7.html')
 
-print('graph_dict:', graph_dict)
+def add_nodes(graph_dict):
+    for num, name in enumerate(graph_dict.keys()):
+        net.add_node(n_id=int(num), label=name, color='pink', size=30, physics=True)
+
+
+def add_edges(graph_dict):
+    for num, adj_nodes in enumerate(graph_dict.values()):
+        print(num)
+        for adj_node in adj_nodes:
+            # print('num', num, type(num), 'adj_node:', adj_node, type(adj_node))
+            # net.add_edge(net.get_node(num), net.get_node(adj_node))
+            try:
+                net.add_edge(num, adj_node, physics=False)
+            except:
+                pass    
+        
+
+
+
+
+if __name__ == '__main__':
+    
+    adj_list = load_adjacency_list('AdjacencyList.txt')
+    adj_list = adj_list[:300]
+    
+    adj_dict = create_adjacency_dictionary(adj_list)
+    
+    net = Network(width='1000px', height='1000px', bgcolor='#222222', font_color='white')
+    net.barnes_hut()
+    
+    add_nodes(adj_dict)
+    add_edges(adj_dict)
+    
+    graph_name = os.getcwd() + '\\graphs\\graph13.html'
+    # net.show(graph_name)
+    try:
+        net.save_graph(graph_name)
+    except:
+        pass
+    
+    
+    
--- DictionaryEditor.py
+++ DictionaryEditor.py
@@ -1,3 +1,5 @@
+# take a dictionary of words and create a textfile of all the words contianing four letters
+
 dict_file = (open('words_alpha.txt')).readlines()
 
 
@@ -6,8 +8,9 @@ for word in dict_file:
     if(len(word) == 5):
         new_dict += word
 
-
+dict_file.close()
 # print(new_dict)
 
 new_dict_file = open('FourLetterWords.txt', 'w')
 new_dict_file.write(new_dict)
+new_dict_file.close()
