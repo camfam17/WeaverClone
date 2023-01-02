5c6b62c2a0336b81546d4a803eecd09516c8642c
Cameron Broomfield <brmcam003@myuct.ac.za>
2023-01-02 22:16:39 +0200

    bfs edge

--- LoadGraph.py
+++ LoadGraph.py
@@ -23,10 +23,11 @@ class LoadGraph():
         
         # self.start_node = self.select_start_word()
         self.start_node = random.randrange(0, len(self.graph.nodes))
+        self.start_node = 1930
         print('start node:', self.start_node)
         
         self.neighbours = self.get_n_steps_neighbours(n=min_guesses)
-        self.print_neighbours()
+        # self.print_neighbours()
     
     
     def select_start_word(self):
@@ -48,22 +49,35 @@ class LoadGraph():
         
         # neighbours[step] = list of neighbours 'step' steps away
         
-        neighbours = [list() for i in range(n)]
-        print('neighbours init', neighbours)
+        # neighbours = [list() for i in range(n)]
+        # print('neighbours init', neighbours)
         
-        neighbours[0].append(self.start_node) # append start node at 0 steps away
-        print('neighbours init2', neighbours)
+        # neighbours[0].append(self.start_node) # append start node at 0 steps away
+        # print('neighbours init2', neighbours)
         
-        for i in range(1, 3):
+        # for i in range(1, 3):
             
-            prev_list = neighbours[i-1]
-            next_list = []
-            for node in prev_list:
-                print(type(node))
-                neis = self.graph.neighbors(node)
-                neis = list(neis)
-                next_list.append(neis)
-            neighbours[i] = next_list
+        #     prev_list = neighbours[i-1]
+        #     next_list = []
+        #     for node in prev_list:
+        #         print(type(node))
+        #         neis = self.graph.neighbors(node)
+        #         neis = list(neis)
+        #         next_list.append(neis)
+        #     neighbours[i] = next_list
+        
+        neighbours = nx.bfs_edges(G=self.graph, source=self.start_node, depth_limit=n)
+        # neighbours = nx.bfs_predecessors(G=self.graph, source=self.start_node, depth_limit=1)
+        print(neighbours)
+        edges = []
+        for x in neighbours:
+            print(x)
+            edges.append(x)
+        
+        new_graph = nx.from_edgelist(edges, self.graph)
+        nx.write_gexf(new_graph, 'smallgraph.gexf')
+        
+        # parse the list of edge-tuples into layers of neighbours
         
         return neighbours
     
